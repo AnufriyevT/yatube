1e79be673cc436ebcdc01e46da4c3ba9cabf237f
AnufriyevT <36229903+AnufriyevT@users.noreply.github.com>
2021-03-02 14:26:21 +0300

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -58,13 +58,8 @@ def profile(request, username):
 
 
 def post_view(request, username, post_id):
-    # тут тело функции
     return render(request, 'post.html', {})
 
 
 def post_edit(request, username, post_id):
-    # тут тело функции. Не забудьте проверить,
-    # что текущий пользователь — это автор записи.
-    # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
-    # который вы создали раньше (вы могли назвать шаблон иначе)
     return render(request, 'post_new.html', {})
